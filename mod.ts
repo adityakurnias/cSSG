589f645f345a2befa990c2899bb3e0110255a92d
adityakurnias <adityakurniasaputra903@gmail.com>
2025-09-13 22:15:21 +0700

    fix(init): Fix not initialize project

--- mod.ts
+++ mod.ts
@@ -1,5 +1,4 @@
 export { build } from "./src/core/build.ts";
 export { init } from "./src/core/init.ts";
 export { loadConfig } from "./src/core/config.ts";
-export * from "./src/cli.ts";
 export type { ResolvedConfig, UserConfig } from "./src/core/config.ts";
